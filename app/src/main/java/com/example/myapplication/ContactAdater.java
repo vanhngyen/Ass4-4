c3e23bdabd6840a9f690d00adf35d84459b6c917
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2021-05-31 22:39:39 +0700

    ass4

--- app/src/main/java/com/example/myapplication/ContactAdater.java
+++ app/src/main/java/com/example/myapplication/ContactAdater.java
@@ -1,4 +1,65 @@
 package com.example.myapplication;
 
-public class ContactAdater {
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ContactAdater extends BaseAdapter {
+
+    private List<ContactModel> listContacts;
+    private Activity activity;
+
+    public ContactAdater(List<ContactModel> listContacts, Activity activity) {
+        this.listContacts = listContacts;
+        this.activity = activity;
+    }
+
+    @Override
+    public int getCount() {
+        return listContacts.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null){
+            LayoutInflater inflater = activity.getLayoutInflater();
+            convertView = inflater.inflate(R.layout.item_contact,parent,false);
+            ViewHolder holder = new ViewHolder();
+            holder.tvName = (TextView)convertView.findViewById(R.id.tvName);
+            holder.tvPhone = (TextView)convertView.findViewById(R.id.tvPhone);
+            holder.ivAvatar = (ImageView)convertView.findViewById(R.id.ivAvatar);
+            convertView.setTag(holder);
+
+        }
+        ViewHolder holder = (ViewHolder) convertView.getTag();
+        ContactModel model = listContacts.get(position);
+        holder.tvName.setText(model.getName());
+        holder.tvPhone.setText(model.getPhone());
+        holder.ivAvatar.setImageResource(model.getAvatar());
+        return convertView;
+    }
+
+    static class ViewHolder{
+        TextView tvName;
+        TextView tvPhone;
+        ImageView ivAvatar;
+    }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lvContact;
+    private List<ContactModel> listContacts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
+        lvContact = (ListView) findViewById(R.id.lvContact);
+        ContactAdater adater = new ContactAdater(listContacts,this);
+        lvContact.setAdapter(adater);
+        lvContact.setOnClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                ContactModel contactModel = listContacts.get(position);
+                Toast.makeText(MainActivity.this,contactModel.getName(),Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+    private void initData() {
+        ContactModel contact = new ContactModel("Nguyen Van A",R.drawable.ic_user_a);
+        listContacts.add(contact);
+        contact = new ContactModel("Nguyen Van B","122627673",R.drawable.ic_user_b);
+        listContacts.add(contact);
+        contact = new ContactModel("Nguyen Van C","122627673",R.drawable.ic_user_c);
+        listContacts.add(contact);
+        contact = new ContactModel("Nguyen Van D","122627673",R.drawable.ic_user_d);
+        listContacts.add(contact);
+        contact = new ContactModel("Nguyen Van E","122627673",R.drawable.ic_user_e);
+        listContacts.add(contact);
+        contact = new ContactModel("Nguyen Van F","122627673",R.drawable.ic_user_f);
+        listContacts.add(contact);
+        contact = new ContactModel("Nguyen Van H","122627673",R.drawable.ic_user_h);
+        listContacts.add(contact);
+        contact = new ContactModel("Nguyen Van G","122627673",R.drawable.ic_user_g);
+        listContacts.add(contact);
+        contact = new ContactModel("Nguyen Van K","122627673",R.drawable.ic_user_k);
+        listContacts.add(contact);
+        contact = new ContactModel("Nguyen Van Q","122627673",R.drawable.ic_user_q);
+
+
+
     }
 }
